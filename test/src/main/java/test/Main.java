2c71f9ae3469409c1422e9f32a837bd2d6539514
Andreas Landerer <laan@zuehlke.com>
2015-07-30 15:56:49 +0200

    Write one million UUIDs

--- test/src/main/java/test/Main.java
+++ test/src/main/java/test/Main.java
@@ -10,7 +10,9 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File("Report.txt")));
-		writer.write(UUID.randomUUID().toString());
+		for (int i = 0; i < 1000000; i++) {
+			writer.write(UUID.randomUUID().toString());
+		}
 		writer.flush();
 		writer.close();
 	}
