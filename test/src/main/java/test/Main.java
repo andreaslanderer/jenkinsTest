b6a0edbe458e9b7ea24066a13e83886a7e348cf3
Andreas Landerer <laan@zuehlke.com>
2015-07-30 16:00:56 +0200

    Write one million UUIDs separated by new lines

--- test/src/main/java/test/Main.java
+++ test/src/main/java/test/Main.java
@@ -12,6 +12,7 @@ public class Main {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File("Report.txt")));
 		for (int i = 0; i < 1000000; i++) {
 			writer.write(UUID.randomUUID().toString());
+			writer.newLine();
 		}
 		writer.flush();
 		writer.close();
